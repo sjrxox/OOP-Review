7ecd638fd43652cfc54b4bfb472e041e16bd29ac
Sharaine Reyes <46492908+sjrxox@users.noreply.github.com>
2019-01-08 09:34:27 -0700

    Earned

--- src/CSharp.Language.Review/EarnedMark.cs
+++ src/CSharp.Language.Review/EarnedMark.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace CSharp.Language.Review
 {
-    class EarnedMark
+    public class EarnedMark
     {
+        public int Possible { get; private set; }
+        private double _Earned;
+        public double Earned
+        {
+            get { return _Earned; }
+            set
+            {
+                if (value < 0 || value > Possible)
+                    throw new Exception("Invalid earned mark assigned");
+                _Earned = value;
+
+            }
+            
+        }
+        public double percent
+        { get { return (Earned / Possible) * 100; } }
+        public double WeightedPercent;
+        
+
     }
 }
