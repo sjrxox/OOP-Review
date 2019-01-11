4784fd8748c3a3338bef7240587cc550285a0ac1
sjrxox <46492908+sjrxox@users.noreply.github.com>
2019-01-11 11:24:51 -0700

    Added comments to program.cs

--- src/CSharp.Language.Review/Program.cs
+++ src/CSharp.Language.Review/Program.cs
@@ -1,21 +1,40 @@
-﻿using System;
+﻿//Identifies the namespaces containing the datatypes 
+//that we want to use or reference in the code in this file
+
+//f12 takes you to where a definition or code is.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 
-
+// declares an area or named-space in which we can 
+// place our programmer-defined data types
 namespace CSharp.Language.Review
 {
+    //the namespace plus the class name is what's called
+    // a "fully-qualified" class name.
+    //the fully-qualified class name for Program is
+    //  CSharp.Language.Review.Program
+
+    //keywords = blue
+    //class = cyan
+    //
     public class Program
     {
-
+        //A static field initialized to a new Random object
         private static Random rnd = new Random();
 
+        // Main() is the entry point
+        //static is needed in Main
+        //static method
         public static void Main(string[] args)
         {
 
+
+            //the body of the Main() method
+            //acts as the "driver" of my application.
             Program app = new Program(args);
 
             app.AssignMarks(30, 80);
@@ -29,14 +48,27 @@ namespace CSharp.Language.Review
             }
         }
 
+        //field
+        //this field is acting as a "backing store"
+        //for the students property
         private List<Student> _students = new List<Student>();
 
+        //property
+        //implemented because it is told what to do
+        
+        //This property provides "controlled access"
+        // to do the data in the backing store (the field)
         public List<Student> Students
         { 
             get { return _students; }
             set { _students = value; }
         }
 
+
+        //constructor
+        //The job of a constructor is to enusre
+        //that all of the fields/properties
+        //have "meaningful" values.
         public Program(string[] studentNames)
         {
             WeightedMark[] CourseMarks = new WeightedMark[4];
@@ -54,7 +86,16 @@ namespace CSharp.Language.Review
                 Students.Add(new Student(name, marks));
             }
         }
+        //XML comment ///
 
+        /// <summary>
+        /// This assigns a random mark to each student 
+        /// in the <see cref="Students"/> property
+        /// </summary>
+        /// <param name="min"> The minimum possible earned value for the student's mark</param>
+        /// <param name="max"> The maximum possible earned value for the student's mark</param>
+
+        //method
         public void AssignMarks(int min, int max)
         {
             foreach (Student person in Students)
